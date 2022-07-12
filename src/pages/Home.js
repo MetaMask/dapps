d311ef6ae027ef839bde79bfa3c76adce8a25378
Andre Pimenta <andrepimenta7@gmail.com>
2022-07-12 18:48:35 +0100

    Show explore sites on iOS (#132)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,6 @@ import ExploreDapps from '../components/ExploreDapps/';
 import Favorites from '../components/Favorites/';
 import TakeATour from '../components/TakeATour/';
 import{trackEvent, ANALYTICS_EVENT_OPTS} from '../util/analytics';
-import { isIOS } from 'react-device-detect';
 
 const isMobile = {
     Android: function () {
@@ -42,7 +41,7 @@ export default class Home extends Component {
                 <Autocomplete />
                 {/* <FeaturedDappsCarousel /> */}
                 <Tabs>
-                    <div label="Explore sites" hide={isIOS}>
+                    <div label="Explore sites">
                         <ExploreDapps />
                     </div>
                     <div label="Favorites">
