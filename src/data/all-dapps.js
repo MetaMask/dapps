f34708d1f33cc398df7df73a35314f00204a9079
sethkfman <Seth.Kaufman@consensys.net>
2022-08-16 14:23:00 -0600

    added iOS import

--- src/data/all-dapps.js
+++ src/data/all-dapps.js
@@ -1,4 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { isIOS } from 'react-device-detect';
 import { faCreditCard,faDice, faCommentAlt, faMoneyBillWave, faNewspaper, faWrench, faExchangeAlt, faStore  } from '@fortawesome/free-solid-svg-icons'
 library.add(faCreditCard, faDice, faCommentAlt, faMoneyBillWave, faNewspaper, faWrench, faExchangeAlt, faStore)
 
