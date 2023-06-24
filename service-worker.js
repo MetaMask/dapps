59aec6c9a8d3bb6fc0e958e82b496a6b0263831c
Mark Stacey <markjstacey@gmail.com>
2023-06-24 00:12:07 -0230

    Update `gh-pages` (#179)
    * Update `gh-pages`
    
    The `gh-pages` branch now uses a build generated from the commit
    84c0020f11799b0e5cc418778333c62e872c75bd
    
    * Fix chainlist image name

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44ac8f94222ea82649f628767324fe5a.js"
+  "./precache-manifest.a0d10558fb43599f1a5d4202fad7ad7e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
