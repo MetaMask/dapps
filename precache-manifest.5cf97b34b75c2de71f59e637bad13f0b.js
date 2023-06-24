fd6e676f3a23a520cf7dba5d6bd569dc5db5dd4c
Mark Stacey <markjstacey@gmail.com>
2023-06-24 00:31:05 -0230

    Update `gh-pages`
    The `gh-pages` branch now uses a build generated from the commit
    ba930fd4e7fbb69b76c235eaffdaefd0f3512b50

---
+++ precache-manifest.5cf97b34b75c2de71f59e637bad13f0b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f30738d41f730937edf0374408cfc3",
+    "url": "/dapps/index.html"
+  },
+  {
+    "revision": "ec3a752af5b2b0cac867",
+    "url": "/dapps/static/css/main.854f8e5d.chunk.css"
+  },
+  {
+    "revision": "e35aaefb5a838b18f302",
+    "url": "/dapps/static/js/2.4bd12f97.chunk.js"
+  },
+  {
+    "revision": "ec3a752af5b2b0cac867",
+    "url": "/dapps/static/js/main.dd8acd42.chunk.js"
+  },
+  {
+    "revision": "4136677860b91275ceef",
+    "url": "/dapps/static/js/runtime~main.16fcfcad.js"
+  },
+  {
+    "revision": "79bb2c6dbf8896e5b6b51029ff418ec7",
+    "url": "/dapps/static/media/back-icon.79bb2c6d.svg"
+  },
+  {
+    "revision": "a24f336a19083949996fcc8ef7c51569",
+    "url": "/dapps/static/media/bg-img.a24f336a.svg"
+  },
+  {
+    "revision": "3fcc47fb61652908aaed3fb4be4c6aab",
+    "url": "/dapps/static/media/close-icon.3fcc47fb.svg"
+  },
+  {
+    "revision": "d74d09840106bd9a39baeeb0f75e7e0a",
+    "url": "/dapps/static/media/logo-wordmark.d74d0984.svg"
+  },
+  {
+    "revision": "d0e2d96de934c523810ef86ac4a29e62",
+    "url": "/dapps/static/media/logo.d0e2d96d.svg"
+  }
+]);
